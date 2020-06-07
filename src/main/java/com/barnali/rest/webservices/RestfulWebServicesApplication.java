be02bd758e2d4676fe5587530f4e6e62b89e47f5
Barnali <mail2barnali1990@gmail.com>
2020-06-07 15:04:56 +0530

    Simplified implementation of Internationalization

--- src/main/java/com/barnali/rest/webservices/RestfulWebServicesApplication.java
+++ src/main/java/com/barnali/rest/webservices/RestfulWebServicesApplication.java
@@ -5,9 +5,8 @@ import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -19,17 +18,9 @@ public class RestfulWebServicesApplication {
 	
 	@Bean
 	public LocaleResolver localeResolver() {
-		SessionLocaleResolver localResolver = new SessionLocaleResolver();
+		AcceptHeaderLocaleResolver localResolver = new AcceptHeaderLocaleResolver();
 		localResolver.setDefaultLocale(Locale.US);
 		return localResolver;
 	}
 	
-	//Bean to read message.properties file
-	@Bean
-	public ResourceBundleMessageSource messageSource() {
-		
-		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-		messageSource.setBasename("message");
-		return messageSource;
-	}
 }
--- src/main/java/com/barnali/rest/webservices/controller/HelloController.java
+++ src/main/java/com/barnali/rest/webservices/controller/HelloController.java
@@ -1,9 +1,8 @@
 package com.barnali.rest.webservices.controller;
 
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,10 +30,9 @@ public class HelloController {
 
 	//consumer will pass locale in header, if header is not passed will be us locale
 	//'required=false' : any value of "Accept-Language" apart from fr and nl will pick the value from message.properties
-	@GetMapping(path = "/hello-world-internalized")
-	public String helloWorldInternalized(@RequestHeader(name="Accept-Language", required=false) Locale locale) {      
-		
-		return messageSource.getMessage("good.morning.message", null, locale);
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {      
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 	
 }
