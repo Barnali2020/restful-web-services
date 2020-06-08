71bf728043161acc3c8107f6b74dc939f10de499
Barnali <mail2barnali1990@gmail.com>
2020-06-08 18:55:44 +0530

    Approach-2: versioning using param,header,produces

--- src/main/java/com/barnali/rest/webservices/controller/VersioningController.java
+++ src/main/java/com/barnali/rest/webservices/controller/VersioningController.java
@@ -10,6 +10,9 @@ import com.barnali.rest.webservices.beans.PersonV2;
 @RestController
 public class VersioningController {
 	
+	/**
+	 * Approach-1: versioning based on uri
+	 */
 	@GetMapping("v1/person")
 	public PersonV1 personV1() {
 		return new PersonV1("Bob Charlie");
@@ -17,6 +20,45 @@ public class VersioningController {
 	
 	@GetMapping("v2/person")
 	public PersonV2 personV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+	
+	/**
+	 * Approach-1: versioning based on request parameter
+	 */
+	@GetMapping(path="/person/param", params="version=1")
+	public PersonV1 paramV1() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(path="/person/param", params="version=2")
+	public PersonV2 paramV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+	
+	/**
+	 * Approach-1: versioning based on header
+	 */
+	@GetMapping(path="/person/headers", headers="X-API-VERSION=1")
+	public PersonV1 headerV1() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(path="/person/headers", headers="X-API-VERSION=2")
+	public PersonV2 headerV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+	
+	/**
+	 * Approach-1: versioning based on produces response
+	 */
+	@GetMapping(path="/person/produces", produces="application/vnd.company.app-v1+json")
+	public PersonV1 producesV1() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(path="/person/produces", produces="application/vnd.company.app-v2+json")
+	public PersonV2 producesV2() {
 		return new PersonV2(new Name("Bob", "Charlie"));
 	}
 
